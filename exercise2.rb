7bf3c037df251719b219ffbc8c75b8929cc73e74
Cleone Jacob <therealcleonejacob@gmail.com>
2016-01-12 15:55:56 -0500

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+#QUESTION NUMBER 1  How would you calculate a good tip for 55 dollar meal?
+
+#CONST
+
+TIP =0.15
+
+#MEAL PRICE
+
+  mealprice = 55
+
+total = mealprice + (mealprice * TIP)
+
+  puts "total price of meal including gratuity is #{total}"
+
+puts "I have " + 9.to_s + " lives "
+
+puts "45628 mulitplied by 7839 is equal to: #{45628 * 7839}  This was used via string interpolation"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
