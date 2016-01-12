ffe9a8936d6a6be4876cb5e35c148c77789c986b
Cleone Jacob <therealcleonejacob@gmail.com>
2016-01-12 18:01:00 -0500

    exercise3 and exercise 3-5

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,18 @@
 puts "what is your name?"
+
 name = gets.chomp
-puts "hello #{name} "  
+
+  puts "hello #{name} "
+
+
+  puts "what is your age?"
+age = gets.chomp.to_i
+puts "you were born in #{2016 - age}"
+
+
+
+one_to_five = (1..5)
+
+one_to_five.each do |num|
+  puts num
+end
