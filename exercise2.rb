3ad0dec75ce473d2c5c8e06494326c3da8334a1d
Cleone Jacob <therealcleonejacob@gmail.com>
2016-01-12 16:31:48 -0500

    name entry

--- exercise2.rb
+++ exercise2.rb
@@ -10,10 +10,17 @@ TIP =0.15
 
 total = mealprice + (mealprice * TIP)
 
+
   puts "total price of meal including gratuity is #{total}"
+
 
 puts "I have " + 9.to_s + " lives "
 
+#String Interpolation
+
 puts "45628 mulitplied by 7839 is equal to: #{45628 * 7839}  This was used via string interpolation"
+
+
+#Is it True of False?
 
 puts (10 < 20 && 30 < 20) || !(10 == 11)
